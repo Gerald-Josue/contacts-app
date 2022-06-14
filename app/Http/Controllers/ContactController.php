ee9eeb6d9003fa9ca49d4ccf36d5f18a78f05b70
Gerald-Josue <gheryking@arch.localdomain>
2022-06-14 09:51:46 -0400

    Validacion de errores 1

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class ContactController extends Controller
 {
@@ -37,7 +38,11 @@ class ContactController extends Controller
     public function store(Request $request)
     {
         //
-        Contact::create($request->all());
+        if (is_null($request->get('name'))){
+            return back()->withErrors([
+                'name' => 'This field is required',
+            ]);
+        }
         return response("Contact Created");
     }
 
